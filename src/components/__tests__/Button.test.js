054df201ae8a4fe35d904d22ecf27e22063d8396
anastely <anastely86@gmail.com>
2020-12-15 18:35:38 +0200

    Mocking TouchableOpacity, to testing Disabled functionality

--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -2,15 +2,40 @@ import React from 'react';
 import {render, fireEvent} from '@testing-library/react-native';
 
 import {Button} from '../Button';
+import {mockComponent} from '../../../testing-config/mockComponent';
+
+jest.mock(
+  'react-native/Libraries/Components/Touchable/TouchableOpacity',
+  () => {
+    return mockComponent(
+      'react-native/Libraries/Components/Touchable/TouchableOpacity',
+      (props) => {
+        return {
+          onPress: props.disabled ? () => {} : props.onPress,
+        };
+      },
+    );
+  },
+);
 
 test('can press the button', () => {
   const onPressMock = jest.fn();
-  const {getByText} = render(<Button text="testing" onPress={onPressMock} />);
+  const {getByText} = render(<Button text="anas" onPress={onPressMock} />);
 
-  fireEvent.press(getByText('testing'));
+  fireEvent.press(getByText('anas'));
   expect(onPressMock).toHaveBeenCalled();
   expect(onPressMock.mock.calls.length).toBe(1);
 
-  fireEvent.press(getByText('testing'));
+  fireEvent.press(getByText('anas'));
   expect(onPressMock.mock.calls.length).toBe(2);
+});
+
+test('onPress is not called when button is disabled', () => {
+  const onPressMock = jest.fn();
+  const {getByText} = render(
+    <Button text="Testing" onPress={onPressMock} disabled={true} />,
+  );
+
+  fireEvent.press(getByText('Testing'));
+  expect(onPressMock).not.toHaveBeenCalled();
 });
---
+++ testing-config/mockComponent.js
@@ -0,0 +1,17 @@
+export const mockComponent = (moduleName, propOverrideFn = () => {}) => {
+  const RealComponent = require.requireActual(moduleName);
+  const React = require('react');
+  const CustomizedComponent = (props) => {
+    return React.createElement(
+      'CustomizedComponent',
+      {
+        ...props,
+        ...propOverrideFn(props),
+      },
+      props.children,
+    );
+  };
+
+  CustomizedComponent.propTypes = RealComponent.propTypes;
+  return CustomizedComponent;
+};
